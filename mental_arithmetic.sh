6e7eafd749c7642ee68a4f1d0c5dd03730fa4b23
husthed <husthed@yeah.net>
2017-09-23 20:40:21 +0800

    complete the codes

--- mental_arithmetic.sh
+++ mental_arithmetic.sh
@@ -203,22 +203,39 @@ function _callMulDiv() {
 
     minN=$1
     maxN=$2
+    num=$3
     c=0
     right=0
     startTime=$(date +%s)
     while [ $c -lt 10 ]
     do
         c=$(($c+1))
-        operand1=$(($(rand $minN $maxN)))
+        if [ $num -eq 1 ]
+        then
+            length=${#_arrayBased10[@]}
+            pos=$(($(rand 1 ${length})-1))
+            operand1=${_arrayBased10[${pos}]}
+            lastPos=$((${length}-1))
+            while [ ${pos} -lt ${lastPos} ]
+            do
+                _arrayBased10[${pos}]=${_arrayBased10[$((${pos}+1))]}
+                pos=$((${pos}+1))
+            done
+            unset _arrayBased10[${lastPos}]
+        else
+            operand1=$(($(rand $minN $maxN)))
+        fi
+
         operand2=$(($(rand $minN $maxN)))
         result=$(echo "${operand1}*${operand2}"|bc)
         operator=$(($(rand $minN $maxN)))
         oddeven=$((operator / 2 * 2)) 
+
         if [ $operator -eq $oddeven ] 
         then
             callCalc ${c} ${operand1} ${operand2} "*"
         else
-            callCalc ${c} ${result} ${operand2} "/"
+            callCalc ${c} ${result} ${operand1} "/"
         fi
 
         right=$(($?+$right))
@@ -236,16 +253,24 @@ function _callMulDiv() {
 function callMulDiv() {
     printf "\t\t乘除\n"
     printf "\t1 乘法口诀（1～10）\n"
-    printf "\t2 综合\n"
+    printf "\t2 两位数乘除法\n"
+    printf "\t3 三位数乘除法\n"
+    printf "\t4 综合\n"
 
-    read -p "请输入选项1或者2: " index
+    read -p "请输入选项1，2, 3或者4: " index
 
     if [ $index == "1" ] 
     then
         _callMul
     elif [ $index == "2" ] 
     then
-        _callMulDiv 2 99
+        _callMulDiv 10 99 1
+    elif [ $index == "3" ] 
+    then
+        _callMulDiv 100 999 1
+    elif [ $index == "4" ] 
+    then
+        _callMulDiv 2 999 2
     else
         echo "选项未知，退出程序"
         exit
@@ -286,7 +311,19 @@ function _doNormal() {
     done
     unset _arrayBased10[${lastPos}]
 
-    callCalc $1 $operand1 $operand2 "*"
+    #callCalc $1 $operand1 $operand2 "*"
+
+    result=$(echo "${operand1}*${operand2}"|bc)
+    operator=$(($(rand 1 999)))
+    oddeven=$((operator / 2 * 2)) 
+
+    if [ $operator -eq $oddeven ] 
+    then
+        callCalc $1 ${operand1} ${operand2} "*"
+    else
+        callCalc $1 ${result} ${operand1} "/"
+    fi
+
     return $?
 }
 
