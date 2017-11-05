f1e643203d027e70f536f299d696e486e6424922
husthed <husthed@yeah.net>
2017-11-05 21:23:10 +0800

    don't exit only press 5 or other key stoken

--- mental_arithmetic.sh
+++ mental_arithmetic.sh
@@ -7,30 +7,34 @@
 
 function _main(){
     clear
-    printf "\t\t口心算\n"
-    printf "\t1 加减\n"
-    printf "\t2 乘除\n"
-    printf "\t3 常规计算\n"
-    printf "\t4 速算\n"
+    while :
+    do
+        printf "\t\t口心算\n"
+        printf "\t1 加减\n"
+        printf "\t2 乘除\n"
+        printf "\t3 常规计算\n"
+        printf "\t4 速算\n"
+        printf "\t5 退出\n"
 
-    read -p "请输入选项1，2, 3或者4: " index
+        read -p "请输入选项1，2, 3或者4: " index
 
-    if [ $index == "1" ] 
-    then
-        callAddSub
-    elif [ $index == "2" ] 
-    then
-        callMulDiv
-    elif [ $index == "3" ] 
-    then
-        callNormal
-    elif [ $index == "4" ] 
-    then
-        callRapid
-    else
-        echo "选项未知，退出程序"
-        exit
-    fi
+        if [ $index == "1" ] 
+        then
+            callAddSub
+        elif [ $index == "2" ] 
+        then
+            callMulDiv
+        elif [ $index == "3" ] 
+        then
+            callNormal
+        elif [ $index == "4" ] 
+        then
+            callRapid
+        else
+            echo "选项未知，退出程序"
+            exit
+        fi
+    done
 }
 
 _main
