bd38254971f57114ca524de766cb56ac6452e21c
husthed <husthed@yeah.net>
2018-07-24 06:58:23 +0800

    add 10x*10y

--- mental_rapid.sh
+++ mental_rapid.sh
@@ -24,13 +24,7 @@ function _callUnitDigit() {
         #done
         #unset _arraySingleDigital[${lastPos}]
 
-        isSame=$3
-        if [ $isSame == 0 ]
-        then
-            callCalc $c $((operand1 * 10 + $1)) $((operand2 * 10 + $1)) "*"
-        else
-            callCalc $c $((operand1 * 10 + $1)) $((operand1 * 10 + $1)) "*"
-        fi
+        callCalc $c $((operand1 * 10 + $1)) $((operand2 * 10 + $1)) "*"
         right=$(($?+$right))
     done
 
@@ -81,7 +75,7 @@ function _callTenDigit() {
 }
 
 ############################
-# 十位数为1/9的乘法速算
+# 补数乘法速算
 ############################
 function _callSupplement() {
     log "$(date)"
@@ -137,6 +131,39 @@ function _callSupplement() {
     echo -e ${LOG_BUFFER} | tee -a ${LOG_FILE_NAME}
 }
 
+############################
+# 给定一个数,任意一个两位数随机
+############################
+function _callRandom() {
+    log "$(date)"
+
+    operand2=$1
+    echo -e $2
+    c=0
+    right=0
+    startTime=$(date +%s)
+    while [ $c -lt 10 ]
+    do
+        c=$(($c+1))
+        length=${#_arraySingleDigital[@]}
+        pos=$(($(rand 1 ${length})-1))
+        i=${_arraySingleDigital[${pos}]}
+        pos=$(($(rand 1 ${length})-1))
+        j=${_arraySingleDigital[${pos}]}
+        operand1=$(echo "${i}*10+${j}"|bc)
+
+        callCalc $c ${operand1} ${operand2} "*"
+        right=$(($?+$right))
+    done
+
+    endTime=$(date +%s)
+    t=$(calTime $startTime $endTime)
+    log "总共10题，正确${right}题, ${t}"
+
+    #tail -n 12 ${LOG_FILE_NAME}
+    echo -e ${LOG_BUFFER} | tee -a ${LOG_FILE_NAME}
+}
+
 #####################################
 # main entry for 乘法速算
 #####################################
@@ -145,21 +172,22 @@ function callRapid() {
     printf "\t1 个位数为1\t2 十位数为1\n"
     printf "\t3 个位数为9\t4 十位数为9\n"
     printf "\t5 头相同,尾互补\t6 尾相同,头互补\n"
-    printf "\t7 互补数乘叠数\t8 x5*x5\n"
+    printf "\t7 互补数乘叠数\t8 xy*99\n"
+    printf "\t9 10x*10y\t10 xy*叠数??????\n"
 
     _arraySingleDigital=(1 2 3 4 5 6 7 8 9)
   
-    read -p "请输入选项1，2, 3, 4, 5, 6或者7: " index
+    read -p "请输入选项1，2, 3, 4, 5, 6, 7, 8或者9: " index
 
     if [ $index == "1" ] 
     then
-        _callUnitDigit 1 "x1*y1的速算口诀:\n头乘头，头加头，尾是1（头加头如果超过10要进位）" 0
+        _callUnitDigit 1 "x1*y1的速算口诀:\n头乘头，头加头，尾是1（头加头如果超过10要进位）"
     elif [ $index == "2" ] 
     then
         _callTenDigit 10 "1x*1y的速算口诀:\n头是1，尾加尾，尾乘尾（超过10要进位）"
     elif [ $index == "3" ] 
     then
-        _callUnitDigit 9 "x9*y9的速算口诀:\n头数各加1 ，相乘再乘10，减去相加数，最后再放1" 0
+        _callUnitDigit 9 "x9*y9的速算口诀:\n头数各加1 ，相乘再乘10，减去相加数，最后再放1"
     elif [ $index == "4" ] 
     then
         _callTenDigit 90 "9x*9y的速算口诀:\n\t方法1:尾数相加再加80作为头,100减大家，结果相互乘,占两位.\n\t方法2:100减前数，再被后数减。100减大家，结果相互乘，占2位"
@@ -174,7 +202,14 @@ function callRapid() {
         _callSupplement 2 "互补数乘叠数速算口诀:\n头加1再乘头，尾乘尾占2位."
     elif [ $index == "8" ] 
     then
-        _callUnitDigit 5 "x5*x5速算公式:\nx5*x5=(x+1)*x*100 + 25(即后两位为25,高位为x与x+1的乘积." 1
+        _callRandom 99 "xy*99速算公式:\nxy*99=(xy-1)*100+(100-xy)=xy*100-xy."
+    elif [ $index == "9" ] 
+    then
+        _callTenDigit 100 "10x*10y速算公式:\n10x*10y=(10x+y)*100+(x*y)."
+    elif [ $index == "10" ] 
+    then
+        #_callRandom  "xy*叠数:\n"
+        echo "Nothing to do"
     else
         echo "选项未知，退出程序"
         exit
